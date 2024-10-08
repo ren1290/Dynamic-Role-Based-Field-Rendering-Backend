2249c2d5ed98036379e19b8ed9b6975a02ac66ab
Renisha <143713752+ren1290@users.noreply.github.com>
2024-10-08 13:30:32 -0400

    Added field type to field table

--- src/main/java/com/example/dynamic/field/rendering/entity/Fields.java
+++ src/main/java/com/example/dynamic/field/rendering/entity/Fields.java
@@ -3,8 +3,6 @@ package com.example.dynamic.field.rendering.entity;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -13,61 +11,71 @@ import jakarta.persistence.OneToMany;
 @Entity
 public class Fields {
 
-	@Id
-	@Column(name = "FIELD_ID")
-	private Integer fieldId;
+    @Id
+    @Column(name = "FIELD_ID")
+    private Integer fieldId;
 
-	@Column(name = "FIELD_NAME")
-	private String fieldName;
+    @Column(name = "FIELD_NAME")
+    private String fieldName;
 
-	@Column(name = "FIELD_DESC")
-	private String fieldDesc;
+    @Column(name = "FIELD_DESC")
+    private String fieldDesc;
 
-	@OneToMany(mappedBy = "field")
-	@JsonManagedReference(value = "field-role-fields")
-	private Set<RoleFields> roleFields;
+    @OneToMany(mappedBy = "field")
+    @JsonManagedReference(value = "field-role-fields")
+    private Set<RoleFields> roleFields;
 
-	public Integer getFieldId() {
-		return fieldId;
-	}
+    @Column(name = "FIELD_TYPE") 
+    private String fieldType;
 
-	public void setFieldId(Integer fieldId) {
-		this.fieldId = fieldId;
-	}
+    public Integer getFieldId() {
+        return fieldId;
+    }
 
-	public String getFieldName() {
-		return fieldName;
-	}
+    public void setFieldId(Integer fieldId) {
+        this.fieldId = fieldId;
+    }
 
-	public void setFieldName(String fieldName) {
-		this.fieldName = fieldName;
-	}
+    public String getFieldName() {
+        return fieldName;
+    }
 
-	public String getFieldDesc() {
-		return fieldDesc;
-	}
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
 
-	public void setFieldDesc(String fieldDesc) {
-		this.fieldDesc = fieldDesc;
-	}
+    public String getFieldDesc() {
+        return fieldDesc;
+    }
 
-	public Set<RoleFields> getRoleFields() {
-		return roleFields;
-	}
+    public void setFieldDesc(String fieldDesc) {
+        this.fieldDesc = fieldDesc;
+    }
 
-	public void setRoleFields(Set<RoleFields> roleFields) {
-		this.roleFields = roleFields;
-	}
+    public Set<RoleFields> getRoleFields() {
+        return roleFields;
+    }
 
-	public Fields(Integer fieldId, String fieldName, String fieldDesc, Set<RoleFields> roleFields) {
-		super();
-		this.fieldId = fieldId;
-		this.fieldName = fieldName;
-		this.fieldDesc = fieldDesc;
-		this.roleFields = roleFields;
+    public void setRoleFields(Set<RoleFields> roleFields) {
+        this.roleFields = roleFields;
+    }
 
-	}
+    public String getFieldType() {
+        return fieldType;
+    }
 
-	public Fields() {
-	}
+    public void setFieldType(String fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public Fields(Integer fieldId, String fieldName, String fieldDesc, String fieldType, Set<RoleFields> roleFields) {
+        this.fieldId = fieldId;
+        this.fieldName = fieldName;
+        this.fieldDesc = fieldDesc;
+        this.fieldType = fieldType; 
+        this.roleFields = roleFields;
+    }
+
+    public Fields() {
+    }
 }
